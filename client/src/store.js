60c89ecfe1005cb4d6f24323aaad92bd8ebe795e
Emanuel Suriano <emanuel.suriano@fdvsolutions.com>
2017-12-20 14:21:13 -0300

    Adding composeWithDevTools

--- client/src/store.js
+++ client/src/store.js
@@ -1,18 +1,16 @@
-import { createStore, applyMiddleware, compose } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { combineReducers } from 'redux';
 import io from 'socket.io-client';
 import createSocketIoMiddleware from 'redux-socket.io';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import reducer from './reducers/reducer';
-
-const reduxDevTools =
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
 
 const socket = io('http://localhost:8000');
 const socketIoMiddleware = createSocketIoMiddleware(socket, 'server/');
 
 export default createStore(
   combineReducers({
-    reducer,
+    reducer
   }),
-  compose(applyMiddleware(socketIoMiddleware), reduxDevTools),
+  composeWithDevTools(applyMiddleware(socketIoMiddleware))
 );
