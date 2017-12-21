c255459020bfb7b01de6d935ef13b2068892c9b8
marcos.mezzabotta <marcos.mezzabotta@intive-fdv.com>
2017-12-21 11:43:02 -0300

    Changing styles to make concistence with the mobile app

--- client/src/components/Arrow/Arrow.js
+++ client/src/components/Arrow/Arrow.js
@@ -3,7 +3,7 @@ import FontAwesome from 'react-fontawesome';
 
 const Arrow = ({ onClick, isGoingDown }) => (
   <div onClick={onClick} style={{ cursor: 'pointer' }}>
-    <FontAwesome name={isGoingDown ? 'arrow-down' : 'arrow-up'} size="4x" />
+    <FontAwesome style={{ color: '#e2e2e2'}} name={isGoingDown ? 'arrow-down' : 'arrow-up'} size="4x" />
   </div>
 );
 
--- client/src/components/RollerBlind/RollerBlind.js
+++ client/src/components/RollerBlind/RollerBlind.js
@@ -2,8 +2,8 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 const INITIAL_HEIGHT = 200;
-const MIN_HEIGHT = 15;
-const MAX_HEIGHT = 315;
+const MIN_HEIGHT = 50;
+const MAX_HEIGHT = 350;
 const TOTAL_HEIGHT = 300;
 
 class RollerBlind extends Component {
@@ -14,12 +14,12 @@ class RollerBlind extends Component {
   };
 
   state = {
-    position: 15,
+    position: MIN_HEIGHT,
   };
 
   componentWillReceiveProps(nextProps) {
     if (!nextProps.isDisable)
-      this.setState({ position: nextProps.height * 3 + 15 });
+      this.setState({ position: nextProps.height * 3.5 + MIN_HEIGHT});
   }
 
   onDrag = event => {
@@ -42,7 +42,8 @@ class RollerBlind extends Component {
   endDrag = event => {
     if (this.props.isDisable) return;
     const position = this.validatePosition(event.clientY - INITIAL_HEIGHT);
-    return this.props.onChange((position - MIN_HEIGHT) / 3);
+    const roundedPosition = Math.round(position/3.5 - MAX_HEIGHT);
+    return this.props.onChange(roundedPosition);
   };
 
   render() {
@@ -53,11 +54,14 @@ class RollerBlind extends Component {
           onDragStart={this.startDrag}
           onDrag={this.onDrag}
           onDragEnd={this.endDrag}
-          className="roller"
+          className="rollerContainer"
           style={{ height: this.state.position }}
           draggable
         >
-          <div className="rollerEnd">...</div>
+          <div className="internalRoller" />
+          <div className="rollerEnd">
+              <div className="dragButton">  .</div>
+          </div>
         </div>
       </div>
     );
